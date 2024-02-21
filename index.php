0c86fcc82a97b6640e4db7321e0724d30efafc0a
Mohsen Ahmadnia <ahmadnia.ce@gmail.com>
2024-02-21 08:36:12 +0330

    -

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include('header.php'); ?>
 
-    <h1 class="text-center alert alert-secondary">خوش آمدید!</h1>
+    <h1 class="text-center alert alert-secondary">خوش آمدید!!!</h1>
+    <h1 class="text-center alert alert-secondary">صفحه اصلی</h1>
 
 <?php include('footer.php'); ?>
