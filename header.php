c64f58c0ead02d6c6133cf35e37241b153121ae6
Mohsen Ahmadnia <ahmadnia.ce@gmail.com>
2024-04-16 10:12:28 +0330

    رفع مشکل امنیتی دسترسی به پوشه های سایت

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>فرشگاه کتاب</title>
+    <title>فروشگاه کتاب</title>
     <link rel="stylesheet" href="bs/bootstrap.rtl.min.css">
     <link rel="stylesheet" href="bs/bootstrap-icons.min.css">
     <link rel="stylesheet" href="styles/myStyle.css">
